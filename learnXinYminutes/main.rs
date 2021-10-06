6f4450b188eb84104a9b4d899a5490ce88c129aa
Erick Quintanilla <equintanilla@berkeley.edu>
2021-10-05 23:47:11 -0700

    Complete pattern matching

--- learnXinYminutes/main.rs
+++ learnXinYminutes/main.rs
@@ -191,5 +191,29 @@ impl<T> Foo<T> {
   /////////////////////////
   // 3. Pattern matching //
   /////////////////////////
+
+  let foo = OptionalI32::AnI32(1);
+  match foo {
+    OptionalI32::AnI32(n) => 
+      println!("it's an i32: {}", n),
+    OptionalI32::Nothing =>
+      println!("it's nothing!"),
+  }
+
+  // Advanced pattern matching
+  struct FooBar { x: i32, y: OptionalI32 }
+  let bar = FooBar { x: 15, y: OptionalI32::AnI32(32) };
+
+  match bar {
+    FooBar { x: 0, y: OptionalI32::AnI32(0) } =>
+      println!("The numbers are zero!"),
+    FooBar { x: n, y: OptionalI32::AnI32(m) } if n == m =>
+      println!("The numbers are the same"),
+    FooBar { x: n, y: OptionalI32::AnI32(m) } =>
+      println!("Different numbers: {} {}", n, m),
+    FooBar { x: _, y: OptionalI32::Nothing } =>
+      println!("The Second number is Nothing!"),
+  }
   
 }
+
