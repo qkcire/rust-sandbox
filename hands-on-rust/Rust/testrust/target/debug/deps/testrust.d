81ef57317fb81b029c25ad054fdaf249f225a4a8
eq <eq@Richards-MacBook-Air.local>
2021-10-06 22:11:36 -0700

    Start hands-on-rust book

---
+++ hands-on-rust/Rust/testrust/target/debug/deps/testrust.d
@@ -0,0 +1,5 @@
+/Users/eq/rust-sandbox/hands-on-rust/Rust/testrust/target/debug/deps/testrust: src/main.rs
+
+/Users/eq/rust-sandbox/hands-on-rust/Rust/testrust/target/debug/deps/testrust.d: src/main.rs
+
+src/main.rs:
---
+++ hands-on-rust/Rust/testrust/target/debug/testrust.d
@@ -0,0 +1,1 @@
+/Users/eq/rust-sandbox/hands-on-rust/Rust/testrust/target/debug/testrust: /Users/eq/rust-sandbox/hands-on-rust/Rust/testrust/src/main.rs
