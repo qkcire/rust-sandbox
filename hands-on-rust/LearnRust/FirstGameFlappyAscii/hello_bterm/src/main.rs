d1b3150eea1ecf4cdfa513f6a54d6cc9568fdbe2
eq <eq@Richards-MacBook-Air.local>
2021-10-08 10:40:46 -0700

    Complete Flappy Dragon

--- hands-on-rust/LearnRust/FirstGameFlappyAscii/hello_bterm/src/main.rs
+++ hands-on-rust/LearnRust/FirstGameFlappyAscii/hello_bterm/src/main.rs
@@ -209,7 +209,7 @@ impl GameState for State {
 
 fn main() -> BError {
     let context = BTermBuilder::simple80x50()
-        .with_title("Flappy Dragon")
+        .with_title("Flappy Dragon!")
         .build()?;
     main_loop(context, State::new())
 }
