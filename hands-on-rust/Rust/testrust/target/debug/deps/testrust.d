6408cef50ca62342af6e33c8b0fd44e14dfa1155
Erick Quintanilla <equintanilla@berkeley.edu>
2021-10-09 12:37:56 -0700

    Restarting project

--- hands-on-rust/Rust/testrust/target/debug/deps/testrust.d
+++
@@ -1,5 +0,0 @@
-/Users/eq/rust-sandbox/hands-on-rust/Rust/testrust/target/debug/deps/testrust: src/main.rs
-
-/Users/eq/rust-sandbox/hands-on-rust/Rust/testrust/target/debug/deps/testrust.d: src/main.rs
-
-src/main.rs:
--- hands-on-rust/Rust/testrust/target/debug/testrust.d
+++
@@ -1,1 +0,0 @@
-/Users/eq/rust-sandbox/hands-on-rust/Rust/testrust/target/debug/testrust: /Users/eq/rust-sandbox/hands-on-rust/Rust/testrust/src/main.rs
