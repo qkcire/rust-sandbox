afb228520b92cc7bfa80634ef36fb20d3c2be88a
Erick Quintanilla <equintanilla@berkeley.edu>
2021-10-09 12:56:59 -0700

    Create visitor class

--- 2d-game-dev-in-rust/LearnRust/treehouse/src/main.rs
+++ 2d-game-dev-in-rust/LearnRust/treehouse/src/main.rs
@@ -1,5 +1,23 @@
 use std::io::stdin;
 
+struct Visitor {
+    name: String,
+    greeting: String,
+}
+
+impl Visitor {
+    fn new(name: &str, greeting: &str) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_string(),
+        }
+    }
+
+    fn greet_visitor(&self) {
+        println!("{}", self.greeting);
+    }
+}
+
 fn what_is_your_name() -> String {
     let mut your_name = String::new();
     stdin()
@@ -11,7 +29,11 @@ fn what_is_your_name() -> String {
 }
 
 fn main() {
-    let visitor_list = ["bert", "steve", "fred"];
+    let visitor_list = [
+        Visitor::new("bert", "Hello Bert, enjoy your treehouse."),
+        Visitor::new("steve", "Hi Steve. Your milk is in the fridge."),
+        Visitor::new("fred", "Wow, who invited fred?"),
+    ]
     println!("What's your name?");
     let name = what_is_your_name();
     println!("Hello, {:?}", name);
