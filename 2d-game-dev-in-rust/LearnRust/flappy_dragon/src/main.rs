7f9b443eda435c33d47c55dc61ce6c660638ef6e
Erick Quintanilla <equintanilla@berkeley.edu>
2021-10-09 14:43:41 -0700

    Implement obstacle

--- 2d-game-dev-in-rust/LearnRust/flappy_dragon/src/main.rs
+++ 2d-game-dev-in-rust/LearnRust/flappy_dragon/src/main.rs
@@ -10,6 +10,17 @@ struct Obstacle {
     size: i32,
 }
 
+impl Obstacle {
+    fn new(x: i32, score: i32) -> Self {
+        let mut random = RandomNumberGenerator::new();
+        Obstacle {
+            x,
+            gap_y: random.range(10, 40),
+            size: i32::max(2, 20 - score)
+        }
+    }
+}
+
 struct Player {
     x: i32,
     y: i32,
