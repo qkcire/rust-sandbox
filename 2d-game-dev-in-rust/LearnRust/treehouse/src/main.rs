ae1293a1c1ff1cec5db1be2275548979b0dfa3ef
Erick Quintanilla <equintanilla@berkeley.edu>
2021-10-09 12:52:17 -0700

    Add feature to filter invited guest and uninvited guest

--- 2d-game-dev-in-rust/LearnRust/treehouse/src/main.rs
+++ 2d-game-dev-in-rust/LearnRust/treehouse/src/main.rs
@@ -6,10 +6,26 @@ fn what_is_your_name() -> String {
         .read_line(&mut your_name)
         .expect("Failed to read line");
     your_name
+        .trim()
+        .to_lowercase()
 }
 
 fn main() {
+    let visitor_list = ["bert", "steve", "fred"];
     println!("What's your name?");
     let name = what_is_your_name();
     println!("Hello, {:?}", name);
+
+    let mut allow_them_in = false;
+    for visitor in &visitor_list {
+        if visitor == &name {
+            allow_them_in = true;
+        }
+    }
+
+    if allow_them_in {
+        println!("Welcome to the Treehouse, {}", name);
+    } else {
+        println!("Sorry, you aren't on the list");
+    }
 }
