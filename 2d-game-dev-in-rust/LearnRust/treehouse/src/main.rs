c63f92ddfab140aa79b62131f8c0e4b938bdc3e8
Erick Quintanilla <equintanilla@berkeley.edu>
2021-10-09 13:29:10 -0700

    Refactor greet visitor method to include enum filters

--- 2d-game-dev-in-rust/LearnRust/treehouse/src/main.rs
+++ 2d-game-dev-in-rust/LearnRust/treehouse/src/main.rs
@@ -25,7 +25,18 @@ impl Visitor {
     }
 
     fn greet_visitor(&self) {
-        println!("{}", self.greeting);
+        match &self.action {
+            VisitorAction::Accept => println!("Welcome to the tree house, {}", self.name),
+            VisitorAction::AcceptWithNote { note } => {
+                println!("Welcome to the treehouse, {}", self.name);
+                println!("{}", note);
+                if self.age < 21 {
+                    println!("Do not server alcohol to {}", self.name);
+                }
+            }
+            VisitorAction::Probation => println!("{} is now a probationary member", self.name),
+            VisitorAction::Refuse => println!("Do not allow {} in!", self.name),
+        }
     }
 }
 
@@ -56,11 +67,11 @@ fn main() {
         match known_visitor {
             Some(visitor) => visitor.greet_visitor(),
             None => {
-                if name.is_empty() {    // (1)
-                    break;  // (2)
+                if name.is_empty() {
+                    break;
                 } else {
                     println!("{} is not on the visitor list.", name);
-                    visitor_list.push(Visitor::new(&name, "New friend"));
+                    visitor_list.push(Visitor::new(&name, VisitorAction::Probation, 0));
                 }
             }
         }
