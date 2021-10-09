393e797632bfa2e0c805c617acd48fc97e73a5d5
Erick Quintanilla <equintanilla@berkeley.edu>
2021-10-09 14:28:16 -0700

    Implement Player and it's render and gravity method

--- 2d-game-dev-in-rust/LearnRust/flappy_dragon/src/main.rs
+++ 2d-game-dev-in-rust/LearnRust/flappy_dragon/src/main.rs
@@ -6,6 +6,37 @@ struct Player {
     velocity: f32,
 }
 
+impl Player {
+    fn new(x: i32, y: i32) -> Self {
+        Player {
+            x,
+            y,
+            velocity: 0.0, 
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(
+            0,
+            self.y,
+            YELLOW,
+            BLACK,
+            to_cp437('Q')
+        );
+    }
+
+    fn gravity_and_move(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += 0.2;
+        }
+        self.y += self.velocity as i32;
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0;
+        }
+    } 
+}
+
 enum GameMode {
     Menu,
     Playing,
