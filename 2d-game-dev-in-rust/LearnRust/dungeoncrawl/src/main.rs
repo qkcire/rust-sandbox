4f0fc8b66fd632d3ffff611d4cc9592fe937c3d1
Erick Quintanilla <equintanilla@berkeley.edu>
2021-10-09 18:25:08 -0700

    Beautify all the things

--- 2d-game-dev-in-rust/LearnRust/dungeoncrawl/src/main.rs
+++ 2d-game-dev-in-rust/LearnRust/dungeoncrawl/src/main.rs
@@ -1,18 +1,20 @@
+#![warn(clippy::pedantic)]
+
 mod map;
 mod map_builder;
 mod player;
 mod camera;
 
 mod prelude {
-  pub use bracket_lib::prelude::*;
-  pub const SCREEN_WIDTH: i32 = 80;
-  pub const SCREEN_HEIGHT: i32 = 50;
-  pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
-  pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
-  pub use crate::map::*;
-  pub use crate::player::*;
-  pub use crate::map_builder::*;
-  pub use crate::camera::*;
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
+    pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
+    pub use crate::map::*;
+    pub use crate::player::*;
+    pub use crate::map_builder::*;
+    pub use crate::camera::*;
 }
 
 use prelude::*;
@@ -28,7 +30,7 @@ impl State {
         let mut rng = RandomNumberGenerator::new();
         let map_builder = MapBuilder::new(&mut rng);
         Self {
-            map: map_builder.map,
+            map : map_builder.map,
             player: Player::new(map_builder.player_start),
             camera: Camera::new(map_builder.player_start)
         }
@@ -48,16 +50,17 @@ impl GameState for State {
 }
 
 fn main() -> BError {
-    let context = BTermBuilder::new()
+    let context = BTermBuilder::new()// (1)
         .with_title("Dungeon Crawler")
         .with_fps_cap(30.0)
-        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
-        .with_tile_dimensions(32, 32)
-        .with_resource_path("resources/")
-        .with_font("dungeonfont.png", 32, 32)
-        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
-        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT) // (2)
+        .with_tile_dimensions(32, 32) // (3)
+        .with_resource_path("/Users/eq/rust-sandbox/2d-game-dev-in-rust/LearnRust/dungeoncrawl/resources/") // (4)
+        .with_font("dungeonfont.png", 32, 32) // (5)
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png") // (6)
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, 
+            "dungeonfont.png") // (7)
         .build()?;
-    
+
     main_loop(context, State::new())
 }
