38eb8c9f58b3dcc375a32030a930177c59b8c94f
Erick Quintanilla <equintanilla@berkeley.edu>
2021-10-10 15:47:26 -0700

    Beautify

--- 2d-game-dev-in-rust/LearnRust/dungeoncrawl/src/map.rs
+++ 2d-game-dev-in-rust/LearnRust/dungeoncrawl/src/map.rs
@@ -39,16 +39,18 @@ impl Map {
         self.in_bounds(point) && self.tiles[map_idx(point.x, point.y)]==TileType::Floor
     }
 
-    fn valid_exit(&self, loc: Point, delta: Point) -> Option<usize> {
-        let destination = loc + detla;
-        if self.in_bounds(destination) {
-            let idx = self.point2d_to_index(destination);
-            Some(idx)
+    fn valid_exit(&self, loc: Point, delta: Point) -> Option<usize> {// (1)
+        let destination = loc + delta;// (2)
+        if self.in_bounds(destination) {// (3)
+            if self.can_enter_tile(destination) {// (4)
+                let idx = self.point2d_to_index(destination);// (5)
+                Some(idx)
+            } else {
+                None// (6)
+            }
         } else {
             None
         }
-    } else {
-        None
     }
 }
 
@@ -56,26 +58,33 @@ impl Algorithm2D for Map {
     fn dimensions(&self) -> Point {
         Point::new(SCREEN_WIDTH, SCREEN_HEIGHT)
     }
+
     fn in_bounds(&self, point: Point) -> bool {
         self.in_bounds(point)
     }
 }
 
-impl BaseMape for Map {
-    fn get_available_exits(&self, idx: usize) -> SmallVec<[(usize, f32); 10]> {
-        let mut exits = SMallVec::new();
-        let location = self.index_to_point2d(idx);
+impl BaseMap for Map {
+    fn get_available_exits(&self, idx: usize)
+    -> SmallVec<[(usize, f32); 10]>// (7)
+    {
+        let mut exits = SmallVec::new();// (8)
+        let location = self.index_to_point2d(idx);// (9)
 
-        if let Some(idx) = self.valid_exit(location, Point::new(-1, 0)) {
+        if let Some(idx) = self.valid_exit(location, Point::new(-1, 0)) {// (10)
+            exits.push((idx, 1.0))// (11)
+        }
+        if let Some(idx) = self.valid_exit(location, Point::new(1, 0)) {// (12)
             exits.push((idx, 1.0))
         }
-        if let Some(idx) = self.valid_exit(location, Point::new(1, 0)) {
-            exits.pus((idx, 1.0))
-        }
         if let Some(idx) = self.valid_exit(location, Point::new(0, -1)) {
-            exits.pus((idx, 1.01))
+            exits.push((idx, 1.0))
         }
-        exits
+        if let Some(idx) = self.valid_exit(location, Point::new(0, 1)) {
+            exits.push((idx, 1.0))
+        }
+
+        exits// (13)
     }
 
     fn get_pathing_distance(&self, idx1: usize, idx2: usize) -> f32 {
