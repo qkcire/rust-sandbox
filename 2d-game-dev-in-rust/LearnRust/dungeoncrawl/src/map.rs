d4c00dffb9ff108cac5c3cdecc09b24703d8441d
Erick Quintanilla <equintanilla@berkeley.edu>
2021-10-09 18:09:16 -0700

    Update map rendering to include camera implementation

--- 2d-game-dev-in-rust/LearnRust/dungeoncrawl/src/map.rs
+++ 2d-game-dev-in-rust/LearnRust/dungeoncrawl/src/map.rs
@@ -23,23 +23,32 @@ pub fn new() -> Self {
     }
 
     pub fn render(&self, ctx: &mut BTerm) {
-        for y in 0..SCREEN_HEIGHT {
-            for x in 0..SCREEN_WIDTH {
-                let idx = map_idx(x, y);
-                match self.tiles[idx] {
-                    TileType::Floor => {
-                        ctx.set(x, y, YELLOW, BLACK, 
-                            to_cp437('.')
-                        );
-                    }
-                    TileType::Wall => {
-                        ctx.set(x, y, GREEN, BLACK, 
-                            to_cp437('#')
-                        );
-                    }
-                }
+      ctx.set_active_console(0);
+      for y in camera.top_y .. camera.right_x {
+        if self.in_bounds(Point::new(x, y)) {
+          let idx = map_idx(x, y);
+          match self.tiles[idx] {
+            TileType::Floor => {
+              ctx.set(
+                x - camera.left_x,
+                y - camera.top_y,
+                WHITE,
+                BLACK,
+                to_cp437('.')
+              );
             }
+            TileType::Wall => {
+              ctx.set(
+                x - camera.left_x,
+                y - camera.top_y,
+                WHITE,
+                BLACK,
+                to_cp437('#')
+              );
+            }
+          }
         }
+      }
     }
 
     pub fn in_bounds(&self, point: Point) -> bool {
