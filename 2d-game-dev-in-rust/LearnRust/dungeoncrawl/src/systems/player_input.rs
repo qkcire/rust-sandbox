c5e543463166b85544299665abd9e769c4a26fd7
Erick Quintanilla <equintanilla@berkeley.edu>
2021-10-10 13:19:18 -0700

    Beautify

--- 2d-game-dev-in-rust/LearnRust/dungeoncrawl/src/systems/player_input.rs
+++ 2d-game-dev-in-rust/LearnRust/dungeoncrawl/src/systems/player_input.rs
@@ -3,15 +3,14 @@ use crate::prelude::*;
 #[system]
 #[read_component(Point)]
 #[read_component(Player)]
+#[read_component(Enemy)]
 pub fn player_input(
-    ecs: &mut SubWorld,
+    ecs: &SubWorld,
     commands: &mut CommandBuffer,
-    #[resource] key: &Option<VirtualKeyCode>,
-    #[resource] turn_state: &mut TurnState
+    #[resource] key : &Option<VirtualKeyCode>,
+    #[resource] turn_state : &mut TurnState
 ) {
-    let mut players = <(Entity, &Point)>::query()
-        .filter(component::<Player>());
-
+    let mut players = <(Entity, &Point)>::query().filter(component::<Player>());
     if let Some(key) = *key {
         let delta = match key {
             VirtualKeyCode::Left => Point::new(-1, 0),
@@ -20,28 +19,37 @@ pub fn player_input(
             VirtualKeyCode::Down => Point::new(0, 1),
             _ => Point::new(0, 0),
         };
+
         let (player_entity, destination) = players
-            .iter(ecs)
-            .find_map( | (entity, pos) | Some((*entity, *pos + delta)) )
-            .unwrap();
-        
+                .iter(ecs)
+                .find_map(|(entity, pos)| Some((*entity, *pos + delta)) )
+                .unwrap();
+
         let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
-        if delta.x != 0 || delta.y != 0 {
+        if delta.x !=0 || delta.y != 0 {
             let mut hit_something = false;
             enemies
                 .iter(ecs)
-                .filter( |(_, pos)| {
+                .filter(|(_, pos)| {
                     **pos == destination
                 })
                 .for_each(|(entity, _) | {
                     hit_something = true;
 
-                    comands
-                        .push((()), WantsToAttack{
+                    commands
+                        .push(((), WantsToAttack{
                             attacker: player_entity,
                             victim: *entity,
                         }));
                 });
+
+            if !hit_something {
+                commands
+                    .push(((), WantsToMove{
+                        entity: player_entity,
+                        destination
+                    }));
+            }
         }
         *turn_state = TurnState::PlayerTurn;
     }
