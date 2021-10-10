8e3fa39726bb545c8a3e6802472c2012575bbf39
Erick Quintanilla <equintanilla@berkeley.edu>
2021-10-10 13:45:19 -0700

    Beautify

--- 2d-game-dev-in-rust/LearnRust/dungeoncrawl/src/systems/player_input.rs
+++ 2d-game-dev-in-rust/LearnRust/dungeoncrawl/src/systems/player_input.rs
@@ -4,14 +4,16 @@ use crate::prelude::*;
 #[read_component(Point)]
 #[read_component(Player)]
 #[read_component(Enemy)]
-#[read_component(Health)]
+#[write_component(Health)]
 pub fn player_input(
-    ecs: &SubWorld,
+    ecs: &mut SubWorld,
     commands: &mut CommandBuffer,
     #[resource] key : &Option<VirtualKeyCode>,
     #[resource] turn_state : &mut TurnState
 ) {
     let mut players = <(Entity, &Point)>::query().filter(component::<Player>());
+    let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
+
     if let Some(key) = *key {
         let delta = match key {
             VirtualKeyCode::Left => Point::new(-1, 0),
@@ -26,9 +28,9 @@ pub fn player_input(
                 .find_map(|(entity, pos)| Some((*entity, *pos + delta)) )
                 .unwrap();
 
-        let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
         let mut did_something = false;
         if delta.x !=0 || delta.y != 0 {
+
             let mut hit_something = false;
             enemies
                 .iter(ecs)
@@ -54,14 +56,14 @@ pub fn player_input(
                         destination
                     }));
             }
-
-            if !did_something {
-                if let Ok(mut health) = ecs
-                    .entry_mut(player_entity)
-                    .unwrap()
-                    .get_component_mut::<Health>() {
-                        health.current = i32::min(health.max, health.current + 1);
-                    }
+        };
+        if !did_something {
+            if let Ok(mut health) = ecs
+                .entry_mut(player_entity)
+                .unwrap()
+                .get_component_mut::<Health>()
+            {
+                health.current = i32::min(health.max, health.current+1);
             }
         }
         *turn_state = TurnState::PlayerTurn;
