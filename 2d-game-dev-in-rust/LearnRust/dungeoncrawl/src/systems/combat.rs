1821376bf1eaf57d57b3880d63eb5c2d284157a4
Erick Quintanilla <equintanilla@berkeley.edu>
2021-10-10 16:06:49 -0700

    Stop the game from crashing when player dies

--- 2d-game-dev-in-rust/LearnRust/dungeoncrawl/src/systems/combat.rs
+++ 2d-game-dev-in-rust/LearnRust/dungeoncrawl/src/systems/combat.rs
@@ -2,6 +2,7 @@ use crate::prelude::*;
 
 #[system]
 #[read_component(WantsToAttack)]
+#[read_component(Player)]
 #[write_component(Health)]
 pub fn combat(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
     let mut attackers = <(Entity, &WantsToAttack)>::query();
@@ -15,15 +16,19 @@ pub fn combat(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
         if let Ok(mut health) = ecs
             .entry_mut(*victim)
             .unwrap()
-            .get_component_mut::<Health>()
-        {
-            println!("Health before attack: {}", health.current);
-            health.current -= 1;
-            if health.current < 1 {
-                commands.remove(*victim);
+            .get_component_mut::<Player>()
+            .is_ok();
+        
+        if let Ok(mut health) = ecs
+            .entry_mut(*victim)
+            .unwrap()
+            .get_component_mut::<Health>() 
+            {
+                healt.current -= 1;
+                if health.current < 1 && !is_Player {
+                    commands.remove(*victim);
+                }
             }
-            println!("Health after attack: {}", health.current);
-        }
         commands.remove(*message);
     });
 }
