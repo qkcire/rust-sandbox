bb23f0f81cf8cf19d25c3471dc70d4ab752cc04f
Erick Quintanilla <equintanilla@berkeley.edu>
2021-10-10 16:31:38 -0700

    Beautify

--- 2d-game-dev-in-rust/LearnRust/dungeoncrawl/src/map_builder.rs
+++ 2d-game-dev-in-rust/LearnRust/dungeoncrawl/src/map_builder.rs
@@ -1,11 +1,11 @@
 use crate::prelude::*;
-const NUM_ROOMS: usize = 20;
 
+const NUM_ROOMS: usize = 20;
 pub struct MapBuilder {
     pub map : Map,
     pub rooms : Vec<Rect>,
     pub player_start : Point,
-    pub amulet_start : Point,
+    pub amulet_start : Point
 }
 
 impl MapBuilder {
@@ -15,12 +15,13 @@ impl MapBuilder {
             map : Map::new(),
             rooms : Vec::new(),
             player_start : Point::zero(),
-            amulet_start : Point::zero(),
+            amulet_start : Point::zero()
         };
         mb.fill(TileType::Wall);
         mb.build_random_rooms(rng);
         mb.build_corridors(rng);
-        mb.player_start = mb.rooms[0].center();// (1)
+        mb.player_start = mb.rooms[0].center();
+
         let dijkstra_map = DijkstraMap::new(
             SCREEN_WIDTH,
             SCREEN_HEIGHT,
@@ -28,15 +29,17 @@ impl MapBuilder {
             &mb.map,
             1024.0
         );
-        const UNREACHABLE : &f32 = &f32::MAX;
-        mb.amulet_start = mb.map.index_to_point2d (
+        const UNREACHABLE : &f32 = &f32::MAX;// (1)
+        mb.amulet_start = mb.map.index_to_point2d// (2)
+        ( 
             dijkstra_map.map
                 .iter()
-                .enumerate()
-                .filter( |(_, dist)| *dist < UNREACHABLE )
-                .max_by( |a,b| a.1.partial_cmp(b.1).unwrap())
-                .unwrap().0
+                .enumerate()// (3)
+                .filter(|(_,dist)| *dist < UNREACHABLE)// (4)
+                .max_by(|a,b| a.1.partial_cmp(b.1).unwrap())// (5)
+                .unwrap().0// (6)
         );
+
         mb
     }
 
@@ -44,25 +47,23 @@ impl MapBuilder {
         self.map.tiles.iter_mut().for_each(|t| *t = tile);
     }
 
-    fn build_random_rooms(&mut self, rng : &mut RandomNumberGenerator) {// (2)
-        while self.rooms.len() < NUM_ROOMS {// (3)
-            let room = Rect::with_size(// (4)
+    fn build_random_rooms(&mut self, rng : &mut RandomNumberGenerator) {
+        while self.rooms.len() < NUM_ROOMS {
+            let room = Rect::with_size(
                 rng.range(1, SCREEN_WIDTH - 10),
                 rng.range(1, SCREEN_HEIGHT - 10),
                 rng.range(2, 10),
                 rng.range(2, 10),
             );
-            let mut overlap = false;// (5)
-            for r in self.rooms.iter() {// (6)
+            let mut overlap = false;
+            for r in self.rooms.iter() {
                 if r.intersect(&room) {
                     overlap = true;
                 }
             }
-            if !overlap {// (7)
+            if !overlap {
                 room.for_each(|p| {
-                    if p.x > 0 && p.x < SCREEN_WIDTH && p.y > 0 
-                        && p.y < SCREEN_HEIGHT 
-                    {
+                    if p.x > 0 && p.x < SCREEN_WIDTH && p.y > 0 && p.y < SCREEN_HEIGHT {
                         let idx = map_idx(p.x, p.y);
                         self.map.tiles[idx] = TileType::Floor;
                     }
@@ -93,13 +94,13 @@ impl MapBuilder {
 
     fn build_corridors(&mut self, rng: &mut RandomNumberGenerator) {
         let mut rooms = self.rooms.clone();
-        rooms.sort_by(|a,b| a.center().x.cmp(&b.center().x));// (8)
+        rooms.sort_by(|a,b| a.center().x.cmp(&b.center().x));
 
-        for (i, room) in rooms.iter().enumerate().skip(1) {// (9)
-            let prev = rooms[i-1].center();// (10)
+        for (i, room) in rooms.iter().enumerate().skip(1) {
+            let prev = rooms[i-1].center();
             let new = room.center();
 
-            if rng.range(0,2) == 1 {// (11)
+            if rng.range(0,2) == 1 {
                 self.apply_horizontal_tunnel(prev.x, new.x, prev.y);
                 self.apply_vertical_tunnel(prev.y, new.y, new.x);
             } else {
