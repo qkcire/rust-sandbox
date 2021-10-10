561bcb8e47229eb9c823c22aebdbf24a66646921
Erick Quintanilla <equintanilla@berkeley.edu>
2021-10-10 12:54:53 -0700

    Drawing tooltips

--- 2d-game-dev-in-rust/LearnRust/dungeoncrawl/src/systems/tooltips.rs
+++ 2d-game-dev-in-rust/LearnRust/dungeoncrawl/src/systems/tooltips.rs
@@ -10,4 +10,23 @@ pub fn tooltips(
   #[resource] camera: &Camera,
 ) {
   let mut positions = <(Entity, &Point, &Name)>::query();
+  let offset = Point::new(camera.left_x, camera.top_y);
+  let map_pos = *mouse_pos + offset;
+  let mut draw_batch = DrawBatch::new();
+  draw_batch.target(2);
+  positions
+    .iter(ecs)
+    .filter(|(_, pos, _)| **pos == map_pos)
+    .for_each( |(entity, _, name)| {
+      let screen_pos = *mouse_pos * 4;
+      let display = if let Ok(health) = ecs.entry_ref(*entity)
+        .unwrap()
+        .get_component::<Health>() {
+          format!("{} : {} hp", &name.0, health.current)
+        } else {
+          name.0.clone()
+        };
+        draw_batch.print(screen_pos, &display);
+    });
+  draw_batch.submit(10100).expect("Batch error");
 }
