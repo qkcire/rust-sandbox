96121f859eb90bfb072c32da84551e1f51ede447
Erick Quintanilla <equintanilla@berkeley.edu>
2021-10-10 15:47:09 -0700

    Add a distance to path function

--- 2d-game-dev-in-rust/LearnRust/dungeoncrawl/src/map.rs
+++ 2d-game-dev-in-rust/LearnRust/dungeoncrawl/src/map.rs
@@ -77,4 +77,12 @@ impl BaseMape for Map {
         }
         exits
     }
+
+    fn get_pathing_distance(&self, idx1: usize, idx2: usize) -> f32 {
+        DistanceAlg::Pythagoras
+            .distance2d(
+                self.index_to_point2d(idx1),
+                self.index_to_point2d(idx2)
+            )
+    }
 }
