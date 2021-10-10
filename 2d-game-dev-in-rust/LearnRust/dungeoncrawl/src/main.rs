dc61c3872aec3fb8d4aafd3a0a9b5dfe8d94e9ed
Erick Quintanilla <equintanilla@berkeley.edu>
2021-10-09 21:39:24 -0700

    Add spawn_player method to State

--- 2d-game-dev-in-rust/LearnRust/dungeoncrawl/src/main.rs
+++ 2d-game-dev-in-rust/LearnRust/dungeoncrawl/src/main.rs
@@ -36,6 +36,7 @@ impl State {
         let mut resources = Resources::default();
         let mut rng = RandomNumberGenerator::new();
         let map_builder = MapBuilder::new(&mut rng);
+        spawn_player(&mut ecs, map_builder.player_start);
         resources.insert(map_builder.map);
         resources.insert(Camera::new(map_builder.player_start));
         Self {
