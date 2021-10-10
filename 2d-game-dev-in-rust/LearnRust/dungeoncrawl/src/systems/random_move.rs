0d80428807d39eefa6744ab20f5eb9696135b6e5
Erick Quintanilla <equintanilla@berkeley.edu>
2021-10-10 11:57:33 -0700

    Refactor to incorporate wants to move messages

--- 2d-game-dev-in-rust/LearnRust/dungeoncrawl/src/systems/random_move.rs
+++ 2d-game-dev-in-rust/LearnRust/dungeoncrawl/src/systems/random_move.rs
@@ -3,22 +3,17 @@ use crate::prelude::*;
 #[system]
 #[write_component(Point)]
 #[read_component(MovingRandomly)]
-pub fn random_move(ecs: &mut SubWorld, #[resource] map: &Map) {// (1)
-    let mut movers = <(&mut Point, &MovingRandomly)>::query();// (2)
-    movers
-        .iter_mut(ecs)
-        .for_each(|(pos, _)| {
-            let mut rng = RandomNumberGenerator::new();
-            let destination = match rng.range(0, 4) {// (3)
-                0 => Point::new(-1, 0),
-                1 => Point::new(1, 0),
-                2 => Point::new(0, -1),
-                _ => Point::new(0, 1),
-            } + *pos;
-
-            if map.can_enter_tile(destination) {// (4)
-                *pos = destination;// (5)
-            }
-        }
-    );
+pub fn random_move(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
+    let mut movers = <(Entity, &Point, &MovingRandomly)>::query();
+    movers.iter(ecs).for_each( | (entity, pos, _) | {
+        let mut rng = RandomNumberGenerator::new();
+        let destination = match rng.range(0, 4) {
+            0 => Point::new(-1, 0),
+            1 => Point::new(1, 0),
+            2 => Point::new(0, -1),
+            _ => Point::new(0, 1),
+        } + *pos;
+        commands
+            .push(((), WantsToMove{ entity: *entity, destination }));
+    });
 }
