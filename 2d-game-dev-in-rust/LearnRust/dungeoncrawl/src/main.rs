307c0c764bdd32b65e0478f0a10958233ca8c3df
Erick Quintanilla <equintanilla@berkeley.edu>
2021-10-10 16:20:16 -0700

    Beautify

--- 2d-game-dev-in-rust/LearnRust/dungeoncrawl/src/main.rs
+++ 2d-game-dev-in-rust/LearnRust/dungeoncrawl/src/main.rs
@@ -61,34 +61,34 @@ impl State {
     }
 
     fn game_over(&mut self, ctx: &mut BTerm) {
-        ctx.set_active_console(2);
-        ctx.print_color_centered(2, RED, BLACK, "Your quest has ended.");
-        ctx.print_color_centered(4, WHITE, BLACK,
+        ctx.set_active_console(2);// (1)
+        ctx.print_color_centered(2, RED, BLACK, "Your quest has ended."); // (2)
+        ctx.print_color_centered(4, WHITE, BLACK, 
             "Slain by a monster, your hero's journey has come to a \
             premature end.");
-        ctx.print_color_centered(5, WHITE, BLACK,
+        ctx.print_color_centered(5, WHITE, BLACK, 
             "The Amulet of Yala remains unclaimed, and your home town \
             is not saved.");
-        ctx.print_color_centered(8, YELLOW, BLACK,
+        ctx.print_color_centered(8, YELLOW, BLACK, 
             "Don't worry, you can always try again with a new hero.");
-        ctx.print_color_centered(9, GREEN, BLACK,
+        ctx.print_color_centered(9, GREEN, BLACK, 
             "Press 1 to play again.");
-        
-            if let Some(VirtualKeyCode::Key1) = ctx.key {
-                self.ecs = World::default();
-                self.resources = Resources::default();
-                let mut rng = RandomNumberGenerator::new();
-                let map_builder = MapBuilder::new(&mut rng);
-                spawn_player(&mut self.ecs, map_builder.player_start);
-                map_builder.rooms
-                    .iter()
-                    .skip(1)
-                    .map( |r| r.center() )
-                    .for_each(|pos| spawn_monster(&mut self.ecs, &mut rng, pos));
-                self.resources.insert(map_builder.map);
-                self.resources.insert(Camera::new(map_builder.player_start));
-                self.resources.insert(TurnState::AwaitingInput);
-            }
+
+        if let Some(VirtualKeyCode::Key1) = ctx.key {// (3)
+            self.ecs = World::default();// (4)
+            self.resources = Resources::default();
+            let mut rng = RandomNumberGenerator::new();
+            let map_builder = MapBuilder::new(&mut rng);
+            spawn_player(&mut self.ecs, map_builder.player_start);
+            map_builder.rooms
+                .iter()
+                .skip(1)
+                .map(|r| r.center())
+                .for_each(|pos| spawn_monster(&mut self.ecs, &mut rng, pos));
+            self.resources.insert(map_builder.map);
+            self.resources.insert(Camera::new(map_builder.player_start));
+            self.resources.insert(TurnState::AwaitingInput);// (5)
+        }
     }
 }
 
