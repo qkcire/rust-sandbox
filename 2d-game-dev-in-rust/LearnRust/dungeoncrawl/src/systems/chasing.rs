30c27b34e3d6db79374596ad144b0fafac070e26
Erick Quintanilla <equintanilla@berkeley.edu>
2021-10-10 15:53:13 -0700

    Beautify and add Dijkstra map

--- 2d-game-dev-in-rust/LearnRust/dungeoncrawl/src/systems/chasing.rs
+++ 2d-game-dev-in-rust/LearnRust/dungeoncrawl/src/systems/chasing.rs
@@ -6,11 +6,22 @@ use crate::prelude::*;
 #[read_component(Health)]
 #[read_component(Player)]
 pub fn chasing(
-  #[resource] map: &Map,
-  ecs: &SubWorld,
-  commands: &mut CommandBuffer,
+    #[resource] map: &Map,
+    ecs: &SubWorld,
+    commands: &mut CommandBuffer
 ) {
-  let mut movers = <(Entity, &Point, &ChasingPlayer)>::query();
-  let mut positions = <(Entity, &Point, &Health)>::query();
-  let mut player = <(&Point, &Player)>::query();
+    let mut movers = <(Entity, &Point, &ChasingPlayer)>::query();
+    let mut positions = <(Entity, &Point, &Health)>::query();
+    let mut player = <(&Point, &Player)>::query();
+    let player_pos = player.iter(ecs).nth(0).unwrap().0;
+    let player_idx = map_idx(player_pos.x, player_pos.y);
+
+    let search_targets = vec![player_idx];// (1)
+    let dijkstra_map = DijkstraMap::new(
+        SCREEN_WIDTH,// (2)
+        SCREEN_HEIGHT,
+        &search_targets,
+        map,// (3)
+        1024.0// (4)
+    );
 }
