3dd60b7bd1b531ca217849294c1df5b0d6fb3515
Erick Quintanilla <equintanilla@berkeley.edu>
2021-10-10 11:59:45 -0700

    Beautify

--- 2d-game-dev-in-rust/LearnRust/dungeoncrawl/src/systems/mod.rs
+++ 2d-game-dev-in-rust/LearnRust/dungeoncrawl/src/systems/mod.rs
@@ -19,7 +19,7 @@ pub fn build_input_scheduler() -> Schedule {
 
 pub fn build_player_scheduler() -> Schedule {
     Schedule::builder()
-        .add_system(movement::movement_sytem())
+        .add_system(movement::movement_system())
         .flush()
         .add_system(collisions::collisions_system())
         .flush()
@@ -34,8 +34,6 @@ pub fn build_monster_scheduler() -> Schedule {
         .add_system(random_move::random_move_system())
         .flush()
         .add_system(movement::movement_system())
-        .flush()
-        .add_system(collisions::collisions_system())
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
--- 2d-game-dev-in-rust/LearnRust/dungeoncrawl/src/systems/player_input.rs
+++ 2d-game-dev-in-rust/LearnRust/dungeoncrawl/src/systems/player_input.rs
@@ -1,17 +1,18 @@
 use crate::prelude::*;
 
 #[system]
-#[write_component(Point)]
+#[read_component(Point)]
 #[read_component(Player)]
 pub fn player_input(
     ecs: &mut SubWorld,
     commands: &mut CommandBuffer,
     #[resource] key: &Option<VirtualKeyCode>,
-    #[resource] turn_sate: &mut TurnState,
+    #[resource] turn_state: &mut TurnState
 ) {
     let mut players = <(Entity, &Point)>::query()
         .filter(component::<Player>());
-    if let Some(key) = key {
+
+    if let Some(key) = *key {
         let delta = match key {
             VirtualKeyCode::Left => Point::new(-1, 0),
             VirtualKeyCode::Right => Point::new(1, 0),
@@ -20,15 +21,11 @@ pub fn player_input(
             _ => Point::new(0, 0),
         };
 
-        if delta.x != 0 || delta.y != 0 {
-            let mut players = <&mut Point>::query()
-                .filter(component::<Player>());
-            players.iter(ecs).for_each( |(entity, pos)| {
-                let destination = *pos + delta;
-                commands
-                    .push(((), WantToMove{ entity: *entity, destination }));
-            });
-            *turn_state = TurnState::PlayerTurn;
-        }
+        players.iter(ecs).for_each(| (entity, pos) | {
+            let destination = *pos + delta;
+            commands
+                .push(((), WantsToMove{ entity: *entity, destination }));
+        });
+        *turn_state = TurnState::PlayerTurn;
     }
 }
