5b86f8821e11e25eca18cd22947daf239f9ba3ea
Erick Quintanilla <equintanilla@berkeley.edu>
2021-10-10 17:32:06 -0700

    rendering from memory

--- 2d-game-dev-in-rust/LearnRust/dungeoncrawl/src/systems/map_render.rs
+++ 2d-game-dev-in-rust/LearnRust/dungeoncrawl/src/systems/map_render.rs
@@ -3,31 +3,53 @@ use crate::prelude::*;
 #[system]
 #[read_component(FieldOfView)]
 #[read_component(Player)]
-pub fn map_render(#[resource] map: &Map, #[resource] camera: &Camera) {// (1)
+pub fn map_render(
+    #[resource] map: &Map,
+    #[resource] camera: &Camera,
+    ecs: &SubWorld
+) {
     let mut fov = <&FieldOfView>::query().filter(component::<Player>());
-    let player_fov = fov.iter(ecs).nth(0).unwrap();
     let mut draw_batch = DrawBatch::new();
     draw_batch.target(0);
+
+    let player_fov = fov.iter(ecs).nth(0).unwrap();
+
     for y in camera.top_y ..= camera.bottom_y {
         for x in camera.left_x .. camera.right_x {
             let pt = Point::new(x, y);
             let offset = Point::new(camera.left_x, camera.top_y);
-            if map.in_bounds(pt) && player_fov.visible_tiles.contains(&pt) {
-                let idx = map_idx(x, y);
-                let glyph = match map.tiles[idx] {
-                    TileType::Floor => to_cp437('.'),
-                    TileType::Wall => to_cp437('#'),
+            let idx = map_idx(x, y);
+            if map.in_bounds(pt) && (player_fov.visible_tiles.contains(&pt) 
+                | map.revealed_tiles[idx]) {// (1)
+                let tint = if player_fov.visible_tiles.contains(&pt) {// (2)
+                    WHITE
+                } else {
+                    DARK_GRAY
                 };
-                draw_batch.set(// (2)
-                    pt - offset,
-                    ColorPair::new(
-                        WHITE,
-                        BLACK
-                    ),
-                    glyph
-                );
+                match map.tiles[idx] {
+                    TileType::Floor => {
+                        draw_batch.set(
+                            pt - offset, 
+                            ColorPair::new(
+                                tint, // (3)
+                                BLACK
+                            ),
+                            to_cp437('.')
+                        );
+                    }
+                    TileType::Wall => {
+                        draw_batch.set(
+                            pt - offset, 
+                            ColorPair::new(
+                                tint, 
+                                BLACK
+                            ),
+                            to_cp437('#')
+                        );
+                    }
+                }
             }
         }
     }
-    draw_batch.submit(0).expect("Batch error");// (3)
+    draw_batch.submit(0).expect("Batch error");
 }
