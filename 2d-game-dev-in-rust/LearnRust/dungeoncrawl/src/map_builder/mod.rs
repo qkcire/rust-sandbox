e8d6221a0de1b5e24551033a3c493cfe617277f0
Erick Quintanilla <equintanilla@berkeley.edu>
2021-10-10 19:06:55 -0700

    Define trait

--- 2d-game-dev-in-rust/LearnRust/dungeoncrawl/src/map_builder/mod.rs
+++ 2d-game-dev-in-rust/LearnRust/dungeoncrawl/src/map_builder/mod.rs
@@ -1,5 +1,9 @@
 use crate::prelude::*;
 
+trait MapArchitect {
+  fn new(&mut self, rng: &mut RandomNumberGenerator) -> MapBuilder;
+}
+
 const NUM_ROOMS: usize = 20;
 pub struct MapBuilder {
     pub map : Map,
